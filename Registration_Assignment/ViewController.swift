0f17fcd54c6a122f88b369ccfdf8a02c1829a355
Radhika <radhikarathi308@gmail.com>
2024-06-15 18:41:25 +0100

    Initial Commit

---
+++ Registration_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registration_Assignment
+//
+//  Created by Rathi, Radhika on 15/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
